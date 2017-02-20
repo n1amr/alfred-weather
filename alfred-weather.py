170c4418a5eb20436300a068eab3394125c4059d
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2017-02-20 22:56:59 +0200

    Getting weather data from server api, viewing in headers

--- alfred-weather.py
+++ alfred-weather.py
@@ -1,13 +1,35 @@
 from alfred.modules.api.a_base_module import ABaseModule
 from alfred.modules.api.a_heading import AHeading
+from datetime import datetime
+import requests
 
 class AlfredWeather(ABaseModule):
+
+    def __init__(self, *args, **kwargs):
+        ABaseModule.__init__(self,*args,**kwargs)
+        self.forecast_data=[]
+
     def callback(self):
-        pass
+        r = requests.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=Cairo&appid=7e73695b9106e411858e94e01532d30d")
+        json_data = r.json()
+
+        for i in range(json_data['cnt']):
+            tmp={'date_time' : datetime.fromtimestamp(json_data["list"][i]['dt']).strftime("%a, %d"),
+                 'max_temp'  : int(json_data["list"][i]['temp']['day'] - 273.15),
+                 'min_temp'  : int(json_data["list"][i]['temp']['night'] - 273.15),
+                 'status'    : json_data["list"][i]['weather'][0]['description']
+                 }
+            self.forecast_data.append(tmp)
+
 
     def construct_view(self):
-        h1 = AHeading(1, "Hello")
-        h2 = AHeading(2, "Bye")
+        for i in range (len(self.forecast_data)):
+            h1 = AHeading(1, "Date: " + str(self.forecast_data[i]['date_time']))
+            h2 = AHeading(2, "Max temp: " + str(self.forecast_data[i]['max_temp']) + "°С")
+            h3 = AHeading(2, "Min temp: " + str(self.forecast_data[i]['min_temp']) + "°С")
+            h4 = AHeading(2, "Status: " + str(self.forecast_data[i]['status']))
 
-        self.add_component(h1)
-        self.add_component(h2)
+            self.add_component(h1)
+            self.add_component(h2)
+            self.add_component(h3)
+            self.add_component(h4)
